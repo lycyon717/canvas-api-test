7a17f9c5efe5f71882746d3466a3537c40db820d
YUIKONNU\liang <420197665@qq.com>
2017-02-20 15:12:40 +0800

    Finish Homework 2

--- out/main.js
+++ out/main.js
@@ -9,58 +9,186 @@ window.onload = function () {
     var stage = new DisplayObjectContainer();
     var text = new TextField();
     text.text = "Hello World!!!!";
-    text.x = 10;
-    text.y = 10;
+    text.x = 20;
+    text.y = 20;
+    text.color = "#00FF00";
+    text.ScaleX = 3;
+    text.rotation = 60;
     var bitmap = new Bitmap();
     bitmap.url = "huli.jpg";
     bitmap.x = 50;
     bitmap.y = 50;
+    bitmap.alpha = 0.5;
+    bitmap.rotation = 30;
     stage.addChild(text);
     stage.addChild(bitmap);
     stage.draw(cxt);
+    setInterval(function () {
+        text.x += 10;
+        bitmap.x += 10;
+        cxt.setTransform(1, 0, 0, 1, 0, 0);
+        cxt.clearRect(0, 0, 800, 800);
+        stage.draw(cxt);
+    }, 500);
 };
-var DisplayObjectContainer = (function () {
-    function DisplayObjectContainer() {
+var DisplayObject = (function () {
+    function DisplayObject() {
+        //本地坐标
         this.x = 0;
         this.y = 0;
+        //透明度
+        this.globalAlpha = 1;
+        this.alpha = 1;
+        this.ScaleX = 1;
+        this.ScaleY = 1;
+        this.Scale = 1;
+        /**
+         *旋转(角度制)
+         */
+        this.rotation = 0;
+    }
+    /**
+     * 调用所有子容器的render方法
+     */
+    DisplayObject.prototype.draw = function (context) {
+        if (this.analysisMatrix(context)) {
+            if (!this.parent) {
+                this.globalAlpha = this.alpha;
+            }
+            else {
+                this.globalAlpha = this.alpha * this.parent.globalAlpha;
+            }
+            this.render(context);
+        }
+        else {
+            console.log("container wrong!");
+            return;
+        }
+    };
+    /**
+     * 子类覆写render方法
+     */
+    DisplayObject.prototype.render = function (context) {
+    };
+    /**
+     * 初始化全局矩阵和本地矩阵,成功则返回true
+     */
+    DisplayObject.prototype.analysisMatrix = function (context) {
+        this.localMatrix = new math.Matrix();
+        this.localMatrix.updateFromDisplayObject(this.x, this.y, this.ScaleX, this.ScaleY, this.rotation);
+        if (!this.parent) {
+            this.globalMatrix = this.localMatrix;
+        }
+        else {
+            this.globalMatrix = math.matrixAppendMatrix(this.parent.globalMatrix, this.localMatrix);
+        }
+        var a = this.globalMatrix.a;
+        var b = this.globalMatrix.b;
+        var c = this.globalMatrix.c;
+        var d = this.globalMatrix.d;
+        var tx = this.globalMatrix.tx;
+        var ty = this.globalMatrix.ty;
+        context.setTransform(a, b, c, d, tx, ty);
+        return true;
+    };
+    return DisplayObject;
+}());
+var DisplayObjectContainer = (function (_super) {
+    __extends(DisplayObjectContainer, _super);
+    function DisplayObjectContainer() {
+        _super.apply(this, arguments);
+        /**
+         * 子容器
+         */
         this.contentsArray = [];
     }
-    DisplayObjectContainer.prototype.addChild = function (newEle) {
-        this.contentsArray.push(newEle);
+    /**
+     * 添加一个子容器
+     */
+    DisplayObjectContainer.prototype.addChild = function (newElement) {
+        var ifAlreadyExist = false;
+        for (var _i = 0, _a = this.contentsArray; _i < _a.length; _i++) {
+            var oldEle = _a[_i];
+            if (oldEle == newElement) {
+                ifAlreadyExist = true;
+                break;
+            }
+        }
+        if (ifAlreadyExist) {
+            console.log("already have same Element");
+        }
+        else {
+            this.contentsArray.push(newElement);
+            newElement.parent = this;
+        }
     };
-    DisplayObjectContainer.prototype.draw = function (context) {
+    /**
+     * 删除指定子容器
+     */
+    DisplayObjectContainer.prototype.removeChild = function (deleteElement) {
+        var ifExist = false;
+        var goalNumber = 0;
+        for (var _i = 0, _a = this.contentsArray; _i < _a.length; _i++) {
+            var del = _a[_i];
+            if (del == deleteElement) {
+                ifExist = true;
+                break;
+            }
+            goalNumber++;
+        }
+        if (!ifExist) {
+            console.log("no element found!!");
+        }
+        else {
+            this.contentsArray.splice(goalNumber, 1);
+        }
+    };
+    DisplayObjectContainer.prototype.render = function (context) {
         for (var _i = 0, _a = this.contentsArray; _i < _a.length; _i++) {
             var content = _a[_i];
             content.draw(context);
         }
     };
     return DisplayObjectContainer;
-}());
+}(DisplayObject));
 var TextField = (function (_super) {
     __extends(TextField, _super);
     function TextField() {
         _super.apply(this, arguments);
         this.text = "";
+        this.color = "#000000";
+        this.font = "15px Arial";
     }
-    TextField.prototype.draw = function (context) {
-        context.fillText(this.text, this.x, this.y);
+    TextField.prototype.render = function (context) {
+        //透明度
+        context.globalAlpha = this.globalAlpha;
+        //填充颜色
+        context.fillStyle = this.color;
+        //文本格式
+        context.font = this.font;
+        //绘制文本
+        context.fillText(this.text, 0, 0);
     };
     return TextField;
-}(DisplayObjectContainer));
+}(DisplayObject));
 var Bitmap = (function (_super) {
     __extends(Bitmap, _super);
     function Bitmap() {
         _super.apply(this, arguments);
+        /**
+         * 图片路径
+         */
         this.url = "";
     }
-    Bitmap.prototype.draw = function (context) {
+    Bitmap.prototype.render = function (context) {
         var _this = this;
         var img = new Image();
         img.src = this.url;
         img.onload = function () {
-            context.drawImage(img, _this.x, _this.y);
+            context.globalAlpha = _this.globalAlpha;
+            context.drawImage(img, 0, 0);
         };
     };
     return Bitmap;
-}(DisplayObjectContainer));
+}(DisplayObject));
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 
-
 window.onload = () => {
 
     var c = document.getElementById("myCanvas") as HTMLCanvasElement;
@@ -9,38 +8,172 @@ window.onload = () => {
 
     var text = new TextField();
     text.text = "Hello World!!!!"
-    text.x = 10;
-    text.y = 10;
+    text.x = 20;
+    text.y = 20;
+    text.color = "#00FF00"
+    text.ScaleX = 3;
+    text.rotation = 60;
 
     var bitmap = new Bitmap();
     bitmap.url = "huli.jpg";
     bitmap.x = 50;
     bitmap.y = 50;
+    bitmap.alpha = 0.5;
+    bitmap.rotation = 30;
 
     stage.addChild(text);
     stage.addChild(bitmap);
 
     stage.draw(cxt);
+
+    setInterval(() => {
+        text.x += 10;
+        bitmap.x += 10;
+        cxt.setTransform(1, 0, 0, 1, 0, 0);
+        cxt.clearRect(0, 0, 800, 800);
+        stage.draw(cxt);
+    }, 500)
 };
 
-interface drawable {
+interface Drawable {
 
     draw(context: CanvasRenderingContext2D);
 }
 
-class DisplayObjectContainer implements drawable {
+class DisplayObject implements Drawable {
 
-    x: number = 0;
-    y: number = 0;
+    //父级容器
+    parent: DisplayObjectContainer;
 
-    contentsArray: DisplayObjectContainer[] = [];
+    //本地坐标
+    x = 0;
+    y = 0;
 
-    addChild(newEle: DisplayObjectContainer) {
-        this.contentsArray.push(newEle);
-    }
+    //透明度
+    protected globalAlpha = 1;
+    alpha = 1;
 
+    ScaleX = 1;
+    ScaleY = 1;
+    Scale = 1;
+
+    /**
+     *旋转(角度制)
+     */
+    rotation = 0;
+
+    //变换矩阵
+    protected globalMatrix: math.Matrix;
+    protected localMatrix: math.Matrix;
+
+    /**
+     * 调用所有子容器的render方法
+     */
     draw(context: CanvasRenderingContext2D) {
 
+        if (this.analysisMatrix(context)) {
+
+            if (!this.parent) {
+                this.globalAlpha = this.alpha;
+            } else {
+                this.globalAlpha = this.alpha * this.parent.globalAlpha;
+            }
+            this.render(context);
+        } else {
+            console.log("container wrong!");
+            return;
+        }
+    }
+
+    /**
+     * 子类覆写render方法
+     */
+    protected render(context: CanvasRenderingContext2D) {
+
+    }
+
+    /**
+     * 初始化全局矩阵和本地矩阵,成功则返回true
+     */
+    protected analysisMatrix(context: CanvasRenderingContext2D): boolean {
+
+        this.localMatrix = new math.Matrix();
+        this.localMatrix.updateFromDisplayObject(this.x, this.y, this.ScaleX, this.ScaleY, this.rotation);
+
+        if (!this.parent) {
+            this.globalMatrix = this.localMatrix;
+        } else {
+            this.globalMatrix = math.matrixAppendMatrix(this.parent.globalMatrix, this.localMatrix);
+        }
+
+        let a = this.globalMatrix.a;
+        let b = this.globalMatrix.b;
+        let c = this.globalMatrix.c;
+        let d = this.globalMatrix.d;
+        let tx = this.globalMatrix.tx;
+        let ty = this.globalMatrix.ty;
+
+        context.setTransform(a, b, c, d, tx, ty);
+        return true;
+    }
+}
+
+class DisplayObjectContainer extends DisplayObject {
+
+    /**
+     * 子容器
+     */
+    contentsArray: DisplayObject[] = [];
+
+
+    /**
+     * 添加一个子容器
+     */
+    addChild(newElement: DisplayObject) {
+
+        let ifAlreadyExist = false;
+
+        for (let oldEle of this.contentsArray) {
+            if (oldEle == newElement) {
+                ifAlreadyExist = true;
+                break;
+            }
+        }
+
+        if (ifAlreadyExist) {
+            console.log("already have same Element");
+        } else {
+
+            this.contentsArray.push(newElement);
+            newElement.parent = this;
+        }
+    }
+
+    /**
+     * 删除指定子容器
+     */
+    removeChild(deleteElement: DisplayObject) {
+
+        let ifExist = false;
+        let goalNumber = 0;
+
+        for (let del of this.contentsArray) {
+            if (del == deleteElement) {
+                ifExist = true;
+                break;
+            }
+            goalNumber++;
+        }
+
+        if (!ifExist) {
+            console.log("no element found!!");
+        } else {
+            this.contentsArray.splice(goalNumber, 1);
+        }
+    }
+
+
+    protected render(context: CanvasRenderingContext2D) {
 
         for (let content of this.contentsArray) {
             content.draw(context);
@@ -48,26 +181,40 @@ class DisplayObjectContainer implements drawable {
     }
 }
 
-class TextField extends DisplayObjectContainer {
 
-    text: string = "";
+class TextField extends DisplayObject {
 
-    draw(context: CanvasRenderingContext2D) {
+    text = "";
+    color = "#000000";
+    font = "15px Arial"
 
-        context.fillText(this.text, this.x, this.y);
+    protected render(context: CanvasRenderingContext2D) {
+
+        //透明度
+        context.globalAlpha = this.globalAlpha;
+        //填充颜色
+        context.fillStyle = this.color;
+        //文本格式
+        context.font = this.font;
+        //绘制文本
+        context.fillText(this.text, 0, 0);
     }
 }
 
-class Bitmap extends DisplayObjectContainer {
+class Bitmap extends DisplayObject {
 
-    url: string = "";
+    /**
+     * 图片路径
+     */
+    url = "";
 
-    draw(context: CanvasRenderingContext2D) {
+    protected render(context: CanvasRenderingContext2D) {
 
         var img = new Image();
         img.src = this.url;
         img.onload = () => {
-            context.drawImage(img, this.x, this.y);
+            context.globalAlpha = this.globalAlpha;
+            context.drawImage(img, 0, 0);
         }
     }
 }
